5eb31f88d4334fb63583a52e4a7948a5fb48fe72
violetrooba <144422810+violetrooba@users.noreply.github.com>
2023-09-08 15:43:02 +0530

    Create Abi.py

---
+++ Abi.py
@@ -0,0 +1,17 @@
+"""
+year%4==0&
+year%100!=0/
+year%400==0
+"""
+def isLeapYear(year):
+   if(year%4==0and year%100
+!=0) or year%400==0:
+     return False
+year=int(input("enter a year:
+"))
+if isLeapYear(year):
+ print('{} is a leap
+year.'.format(year))
+else:
+ printf({} is not a leap
+year.'.fornat(year))
